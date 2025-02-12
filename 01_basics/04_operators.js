c2eb3114209786cfea3edea3e41c5284def5ba3f
Deependra Parmar <dparmar@qodeleaf.com>
2025-02-12 06:19:05 +0530

    Operators

---
+++ 01_basics/04_operators.js
@@ -0,0 +1,11 @@
+let v1 = 10
+let v2 = 2
+
+let str1 = "Hello"
+let str2 = "Deependra"
+
+// Arithmetic operators: +,-,*,/,%,**
+
+console.log('1' + 2 + 2); // 122
+console.log(1 + '2' + 2); // 122
+console.log(1 + 2 + '2'); // 32

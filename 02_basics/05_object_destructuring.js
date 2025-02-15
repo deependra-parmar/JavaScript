ad4c0295e744023718a99cf8ce9841a2dfb24817
Deependra Parmar <dparmar@qodeleaf.com>
2025-02-15 18:06:56 +0530

    Object destructuring

---
+++ 02_basics/05_object_destructuring.js
@@ -0,0 +1,8 @@
+const course = {
+    name: "Javascript in Hindi",
+    price: "999",
+    instructor: "Ironman"
+}
+
+const {name, price, instructor} = course;
+console.log(name, price, instructor);

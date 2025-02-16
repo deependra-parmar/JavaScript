43ef8759acacacb788ccefa8b833b2cdd383e215
Deependra Parmar <dparmar@qodeleaf.com>
2025-02-16 16:33:42 +0530

    For loops

---
+++ 06_iterations/01_for.js
@@ -0,0 +1,21 @@
+/*
+
+for (let index = 0; index <= 10; index++) {
+    console.log(index);
+
+    if(index == 5){
+        console.log("5 is the best number");
+        break;
+    }
+}
+
+*/
+
+const arr = Array.from("Deependra")
+
+for (let index = 0; index < arr.length; index++) {
+    const element = arr[index];
+    console.log(element);
+}
+
+// break for breaking from the loop and continue for skipping an iteration

0697d6c33839df054a7aa5a9c84986b763e74f87
Deependra Parmar <dparmar@qodeleaf.com>
2025-02-16 16:46:55 +0530

    For of

---
+++ 06_iterations/03_higher_order_loops.js
@@ -0,0 +1,16 @@
+// for of loop: 
+
+const arr = [1, 2, 3, 4, 5, 6]
+
+for (const element of arr) {
+    // console.log(element); 
+}
+
+const greeting = "Good morning"
+
+for (const char of greeting) {
+    console.log(char);
+}
+
+// for of can be used to iterate over maps. For of cannot be used to iterate over the objects. It says that object is not iterable. For doing so, we have some different loop.
+

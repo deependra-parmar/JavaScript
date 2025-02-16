732bae368dd7222c7fc6f184247b9afb7b9d2561
Deependra Parmar <dparmar@qodeleaf.com>
2025-02-16 16:02:44 +0530

    If else

---
+++ 05_conditionals/01_if_else.js
@@ -0,0 +1,12 @@
+// execute a condition based on a condition
+
+if(true)
+    console.log("If is executed");
+    console.log("I am true")
+
+if(2 == '2')
+    console.log("2 is equal to 2");
+
+if(2 === '2')
+    console.log("2 is not strict equal to 2");
+

220982f2d738a63701bb1ff703d26d3932b55d15
Deependra Parmar <dparmar@qodeleaf.com>
2025-02-17 05:07:14 +0530

    Async JS

---
+++ 07_asyncjs/01_asyncjs.js
@@ -0,0 +1,10 @@
+/*
+JavaScript by default is a:
+    1. Synchronous language
+    2. Single threaded language
+
+Blocking code vs Non-Blocking code
+    1. Blocking: Blocks the flow of code: Read File Sync
+    2. Non-Blocking: Doesn't block the flow of code: Read File Async
+
+*/
